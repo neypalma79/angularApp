b85c7405c18cb6fd49be12376a5e803bf29be640
Ney Palma <ney_palma@yahoo.com>
2023-07-14 10:03:18 -0500

    agregado routing para el modulo de autorizacion

--- src/app/modules/autorizacion/autorizacion.module.ts
+++ src/app/modules/autorizacion/autorizacion.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsuarioModule } from '../usuario/usuario.module';
+import { AutorizacionRoutingModule } from './autorizacion-routing.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, UsuarioModule],
+  imports: [CommonModule, UsuarioModule, AutorizacionRoutingModule],
   exports: [DashboardComponent],
 })
 export class AutorizacionModule {}
