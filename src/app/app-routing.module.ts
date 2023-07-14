977847595394e1398b406bac821ab3186ad4a5f7
Ney Palma <ney_palma@yahoo.com>
2023-07-14 16:03:56 -0500

    agregado routing modulo general, agregado path hijo dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/usuario/login/login.component';
 import { ErrorComponent } from './modules/general/error/error.component';
 import { DashboardComponent } from './modules/autorizacion/dashboard/dashboard.component';
+import { PerfilComponent } from './modules/usuario/perfil/perfil.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [{ path: 'perfil', component: PerfilComponent }],
+  },
   { path: '**', component: ErrorComponent },
 ];
 
--- src/app/modules/general/general.module.ts
+++ src/app/modules/general/general.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorComponent } from './error/error.component';
+import { GeneralRoutingModule } from './general-routing.module';
 
 @NgModule({
-  declarations: [
-    ErrorComponent
-  ],
-  imports: [CommonModule],
+  declarations: [ErrorComponent],
+  imports: [CommonModule, GeneralRoutingModule],
+  exports: [ErrorComponent],
 })
 export class GeneralModule {}
