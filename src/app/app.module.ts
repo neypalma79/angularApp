ad803e3e6c182a591000544897dd10d87e3032ce
Ney Palma <ney_palma@yahoo.com>
2023-07-28 14:49:54 -0500

    cambio en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutorizacionModule } from './modules/autorizacion/autorizacion.module';
+import { GeneralModule } from './modules/general/general.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AutorizacionModule],
+  imports: [BrowserModule, AppRoutingModule, AutorizacionModule, GeneralModule],
   providers: [],
   bootstrap: [AppComponent],
 })
